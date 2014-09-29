f1dbb6e328cc6b1842dca28658ad6dc65b35ec53
Yu-Hsin Kuo <yuhsink@cs.cmu.edu>
2014-09-29 00:27:53 -0400

    finishing step3

--- src/StepThree.java
+++ src/StepThree.java
@@ -1,4 +1,56 @@
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
 
-public class StepThree {
 
+
+
+/*Start trying step3*/
+
+public class StepThree implements Serializable{
+	
+	private int value, id;
+	public StepThree(int value, int id) {
+		this.value = value;
+		this.id = id;
+	}
+	public int getValue () {
+		return value;
+	}
+	
+	public int getId () {
+		return id;
+	}
+	
+	public static void main (String [] args) {
+		
+
+		try {
+			//writing the object to a file
+			ObjectOutputStream output = new ObjectOutputStream(new FileOutputStream("test.out"));
+			StepThree object = new StepThree(10, 3);
+			output.writeObject(object);
+			output.close();
+			System.out.println(object.getValue());
+			
+			//recreating it
+			ObjectInputStream input = new ObjectInputStream(new FileInputStream("test.out"));
+			Object recreatingObj = input.readObject();
+			System.out.println(((StepThree) recreatingObj).getValue());
+			input.close();
+			
+			
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+
+	}
 }
