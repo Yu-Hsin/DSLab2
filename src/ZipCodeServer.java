e3318d51b03aee4067b8d81d5a03d22b49907c53
Hao-Ping Ho <Jas@Hao-Pings-MacBook-Pro.local>
2014-10-04 21:41:45 -0400

    add some zip

---
+++ src/ZipCodeServer.java
@@ -0,0 +1,7 @@
+
+public interface ZipCodeServer extends Remote440 {
+	public void initialise(ZipCodeList newlist);
+    public String find(String city);
+    public ZipCodeList findAll();
+    public void printAll();
+}
